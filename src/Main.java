a728fa0ddf1d01ee3e717629acdf92510addd56f
vysnoo <vishnukumaresan21@gmail.com>
2026-02-24 18:03:58 +0530

    static method  override?

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        // String pool checking
-
-        String str1 = "hey";
-        String str2 = "hey";
-
-        System.out.println(str1==str2);
-
-        String s1 = new String("Hi");
-        String s2 = new String("Hi");
-
-        System.out.println(s1==s2);
-
-        System.out.println(str1);
-        System.out.println(str2);
-
-        
+        // static method  override?
+        Parent p = new Child();
+        p.show(); // Parent
     }
 }
+
+class Parent {
+    static void show() {
+        System.out.println("Parent");
+    }
+}
+
+class Child extends Parent {
+    static void show() {
+        System.out.println("Child");
+    }
+}
+
+
 
