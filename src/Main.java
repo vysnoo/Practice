0a528af7b5f9cd7f334039e823e3e0f0d615d4ed
vysnoo <vishnukumaresan21@gmail.com>
2026-02-24 17:34:02 +0530

    string pool check

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
 public class Main {
+    public static void main(String[] args) {
+        // String pool checking
+
+        String str1 = "hey";
+        String str2 = "hey";
+
+        System.out.println(str1==str2);
+
+        String s1 = new String("Hi");
+        String s2 = new String("Hi");
+
+        System.out.println(s1==s2);
+
+        System.out.println(str1);
+        System.out.println(str2);
+
+        
+    }
 }
+
