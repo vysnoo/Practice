ca6f5defcf1a01cad981a923939b8f89fccfadfd
vysnoo <vishnukumaresan21@gmail.com>
2026-03-03 18:16:40 +0530

    Linked List Basics

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,42 @@
 public class Main {
     public static void main(String[] args) {
+        // Linked List Practice
+
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+
+        node1.next=node2;
+        node2.next=node3;
+
+
+        Node n = new Node(5,node1);
+
+
+        while(n!=null){
+            System.out.println(n.data);
+            n=n.next;
+        }
+
 
     }
+    static class Node{
+        private int data;
+        private Node next;
+
+        public Node(int data,Node next){
+            this.data= data;
+            this.next = next;
+        }
+        public Node(int data){
+            this.data= data;
+        }
+
+    }
+
+
+
+
 }
 
 
