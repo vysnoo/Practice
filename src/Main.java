5f1ea3827f86471664a718d6f76da3131e71c90d
vysnoo <vishnukumaresan21@gmail.com>
2026-03-04 16:25:25 +0530

    Pattern Practice

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        // Linked List Practice
+        // Patten Program Practice
+        int n = 5;
+        for (int row = 1; row <= n*2-1; row++) {
+            int spaceCond = row<=n*2/2 ? n-row : row-n;
+            for (int space = 1; space <= spaceCond; space++) {
+                System.out.print(" ");
+            }
+            int starCond = row<=n*2/2? (row*2)-1 :(n*2-row)*2-1 ;
+            for (int star = 1; star <= starCond; star++) {
+                if(star==1 || star == starCond){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
 
-        Node node1 = new Node(1);
-        Node node2 = new Node(2);
-        Node node3 = new Node(3);
-
-        node1.next=node2;
-        node2.next=node3;
-
-
-        Node n = new Node(5,node1);
-
-
-        while(n!=null){
-            System.out.println(n.data);
-            n=n.next;
+            }
+            System.out.println();
         }
-
-
     }
-    static class Node{
-        private int data;
-        private Node next;
-
-        public Node(int data,Node next){
-            this.data= data;
-            this.next = next;
-        }
-        public Node(int data){
-            this.data= data;
-        }
-
-    }
-
-
-
-
 }
+
+
 
 
 
