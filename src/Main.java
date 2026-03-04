6ada8d7d9fd3a0c0411e741d1df79108765b1d0e
vysnoo <vishnukumaresan21@gmail.com>
2026-03-04 17:02:59 +0530

    Pattern Practice

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         // Patten Program Practice
-        int n = 6;
+        int n = 5;
         for (int row = 1; row <= n; row++) {
-            for (int col = 1; col <= n; col++) {
-                int min = Math.min(row,Math.min(col,Math.min(n-col+1,n-row+1)));
-                System.out.print(min);
+            for (int space = 1; space <= n - row; space++) {
+                System.out.print(" ");
+            }
+            for (int star = 1; star <= row * 2 - 1; star++) {
+                if (row == n) {
+                    System.out.print("*");
+                } else if (star == 1 || star == row * 2 - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
