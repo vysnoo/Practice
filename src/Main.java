0a71059b4f130931b81cadbd93c4c4639e9e4974
vysnoo <vishnukumaresan21@gmail.com>
2026-03-04 16:29:31 +0530

    Pattern Practice

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,14 @@ public class Main {
     public static void main(String[] args) {
         // Patten Program Practice
         int n = 5;
-        for (int row = 1; row <= n*2-1; row++) {
-            int spaceCond = row<=n*2/2 ? n-row : row-n;
+        for (int row = 1; row <= n * 2 - 1; row++) {
+            int spaceCond = row <= n * 2 / 2 ? n - row : row - n;
             for (int space = 1; space <= spaceCond; space++) {
                 System.out.print(" ");
             }
-            int starCond = row<=n*2/2? (row*2)-1 :(n*2-row)*2-1 ;
+            int starCond = row <= n * 2 / 2 ? (row * 2) - 1 : (n * 2 - row) * 2 - 1;
             for (int star = 1; star <= starCond; star++) {
-                if(star==1 || star == starCond){
-                    System.out.print("*");
-                }else{
-                    System.out.print(" ");
-                }
-
+                System.out.print("*");
             }
             System.out.println();
         }
