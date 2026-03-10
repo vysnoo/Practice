8066ce5eb83f4bc040fecfb555c6df3b1803aec5
vysnoo <vishnukumaresan21@gmail.com>
2026-03-10 12:02:46 +0530

    enum

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
-        // Patten Program Practice
-        int n = 5;
-        for (int row = 1; row <= n; row++) {
-            for (int space = 1; space <= n - row; space++) {
-                System.out.print(" ");
-            }
-            for (int star = 1; star <= row * 2 - 1; star++) {
-                if (row == n) {
-                    System.out.print("*");
-                } else if (star == 1 || star == row * 2 - 1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+public class Main extends Object{
+
+    public static void main(String... args) {
+        //enum
+        Day day = Day.Sunday;
+        System.out.println(day);
     }
+}
+
+enum Day{
+    Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday
 }
 
 
