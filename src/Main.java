7d44ae21b79c19275863208833d68f1e81e2ea86
vysnoo <vishnukumaresan21@gmail.com>
2026-03-13 10:39:05 +0530

    LocalDate

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Main extends Object{
 
     public static void main(String... args) {
         //LocalDate
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
 
-        LocalDate date = LocalDate.parse("2000-02-26");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date = LocalDate.parse(input,dtf);
         System.out.println(date);
     }
 }
