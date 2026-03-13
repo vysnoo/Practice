b3c64e89a56c40a1230aa7971f1e199b5c927263
vysnoo <vishnukumaresan21@gmail.com>
2026-03-13 10:27:58 +0530

    LocalDate

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.time.LocalDate;
+
 public class Main extends Object{
 
     public static void main(String... args) {
-        //enum
-        Day day = Day.Sunday;
-        System.out.println(day);
+        //LocalDate
+
+        LocalDate date = LocalDate.parse("2000-02-26");
+        System.out.println(date);
     }
 }
 
